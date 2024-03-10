b0e0d2a170ce4015cad40603e08a66bb963a5323
ShIIIrochka <shiroooooo@yandex.ru>
2024-03-10 17:42:56 +0300

    поправила багу с коммандами

--- telegram_bots/commands/newrem.py
+++ telegram_bots/commands/newrem.py
@@ -2,7 +2,6 @@ import telebot
 from telebot import types
 from constants import bot
 
-@bot.message_handler(commands=['newrem'])
 def new_reminder(message):
 
 	keyboard = types.InlineKeyboardMarkup()
@@ -14,7 +13,6 @@ def new_reminder(message):
 	keyboard.add(key_no)
 	bot.send_message(message.from_user.id, text='поставить новое напоминание?', reply_markup=keyboard)
 	
-@bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "yes": 
         bot.send_message(call.message.chat.id, text='на какую дату поставить напоминание? \nвведи в формате дд.мм.гггг')
--- telegram_bots/main.py
+++ telegram_bots/main.py
@@ -1,24 +1,26 @@
 import telebot
 from constants import bot
-import sys
-
-sys.path.append('./commands')
-
-from help_start import func_help, send_list_of_commands
-from newrem import new_reminder
 
 bot_token = telebot.TeleBot(bot)
 
 @bot.message_handler(commands=['help', 'start'])
 def handle_help(message):
+    from commands.help_start import func_help
     func_help(message)
 
 @bot.message_handler(commands=['newrem'])
 def handle_newrem(message):
+    from commands.newrem import new_reminder, callback_worker
     new_reminder(message)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    from commands.newrem import callback_worker
+    callback_worker(call)
 
 @bot.message_handler(content_types=['text'])
 def not_command(message):
+    from commands.help_start import send_list_of_commands
     send_list_of_commands(message)
         
 if __name__ == "__main__":
