2b43aa16691770942e6c62a40906edef11351bee
ShIIIrochka <shiroooooo@yandex.ru>
2024-06-26 12:35:24 +0300

    [*] исправила гитигнор, пишу заново newrem.py, добавила новое поле в модель

--- kernel/services/__init__.py
+++ kernel/services/__init__.py
@@ -1,1 +1,3 @@
 # -*- coding: utf-8 -*-
+
+from .engine_service import engine
--- kernel/services/engine_service.py
+++ kernel/services/engine_service.py
@@ -4,3 +4,4 @@ from sqlalchemy import  create_engine
 
 engine = create_engine('sqlite:///sqlite3.db')
 engine.connect()
+
