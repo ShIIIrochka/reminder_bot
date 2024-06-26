b87af4ab40afc800b9a41a069d6103e8c6bb68fb
ShIIIrochka <shiroooooo@yandex.ru>
2024-06-26 13:03:46 +0300

    [+] добавила словарь для записи промежуточных данных пользователя

--- kernel/commands/newrem.py
+++ kernel/commands/newrem.py
@@ -5,8 +5,10 @@ from telebot import types
 import threading
 from datetime import datetime
 
+user_data:dict = {}
+
 def newrem_command(message):
-    chat_id = message.chat.id
+    chat_id:int = message.chat.id
     bot.send_message(chat_id, "Введите название напоминания:")
     bot.register_next_step_handler(message, process_name_step)
 
