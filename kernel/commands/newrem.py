2a11ef96767bf3d3f6729ce65d7bb215b1d74d87
ShIIIrochka <shiroooooo@yandex.ru>
2024-06-26 13:15:21 +0300

    [+] переформировала функции в класс

--- kernel/commands/newrem.py
+++ kernel/commands/newrem.py
@@ -5,16 +5,23 @@ from telebot import types
 import threading
 from datetime import datetime
 
-user_data:dict = {}
 
-def newrem_command(message):
-    chat_id:int = message.chat.id
-    bot.send_message(chat_id, "Введите название напоминания:")
-    bot.register_next_step_handler(message, process_name_step)
+class NewReminder():
+    ''' класс для записи нового напоминания '''
 
-def process_name_step(message):
-    chat_id:int = message.chat.id
-    name:str = message.text
-    user_data[name] = name
-    bot.send_message(chat_id, "Введите описание напоминания:")
-    bot.register_next_step_handler(message, process_description_step)
+    def __init__(self, message):
+        self.user_data:dict = {}
+        self.chat_id:int = message.chat.id
+
+    def newrem_command(self, message):
+        ''' функция для обработки последующих функция'''
+        bot.send_message(self.chat_id, "Введите название напоминания:")
+        bot.register_next_step_handler(message, process_name_step)
+
+    def process_name_step(self, message):
+        ''' функция для обработки названия напоминания'''
+
+        name:str = message.text
+        self.user_data[name] = name
+        bot.send_message(self.chat_id, "Введите описание напоминания:")
+        bot.register_next_step_handler(message, process_description_step)
