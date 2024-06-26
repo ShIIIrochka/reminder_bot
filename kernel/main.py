2077959b54601b5f535fa3a7fff08518e652ddc3
ShIIIrochka <shiroooooo@yandex.ru>
2024-06-26 12:43:01 +0300

    [~] поправила импорты в main.py

--- kernel/main.py
+++ kernel/main.py
@@ -1,33 +1,33 @@
 import telebot
 from constants import bot
 
+from commands.newrem import new_reminder
+from commands.newrem import callback_worker
+from commands.newrem import report
+
+from commands.help_start import func_help
+from commands.help_start import send_list_of_commands
+
 bot_token = telebot.TeleBot(bot)
 
 @bot.message_handler(commands=['help', 'start'])
 def handle_help(message):
-    from commands.help_start import func_help
     func_help(message)
 
 @bot.message_handler(content_types=['text'])
 def not_command(message):
-    from commands.help_start import send_list_of_commands
     send_list_of_commands(message)
 
 @bot.message_handler(commands=['newrem'])
 def handle_newrem(message):
-    from commands.newrem import new_reminder
     new_reminder(message)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-    from commands.newrem import callback_worker
-
     callback_worker(call)
 
 @bot.message_handler(content_types=['text'])
 def date(message):
-    from commands.newrem import report
-
     report(message)
         
 if __name__ == "__main__":
