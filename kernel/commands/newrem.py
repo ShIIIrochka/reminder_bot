3585d2ffdc1298c71897c5d44aa2771d3060713c
ShIIIrochka <shiroooooo@yandex.ru>
2024-06-26 13:06:12 +0300

    [+] добавила фукнцию для обработки названия напоминания

--- kernel/commands/newrem.py
+++ kernel/commands/newrem.py
@@ -12,3 +12,9 @@ def newrem_command(message):
     bot.send_message(chat_id, "Введите название напоминания:")
     bot.register_next_step_handler(message, process_name_step)
 
+def process_name_step(message):
+    chat_id:int = message.chat.id
+    name:str = message.text
+    user_data[name] = name
+    bot.send_message(chat_id, "Введите описание напоминания:")
+    bot.register_next_step_handler(message, process_description_step)
