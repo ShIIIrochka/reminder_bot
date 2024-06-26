8814fc0b674de52c6769489efa70d9ffe252a208
ShIIIrochka <shiroooooo@yandex.ru>
2024-06-26 13:20:32 +0300

    [*] поправила отступы в constants.py

--- kernel/constants.py
+++ kernel/constants.py
@@ -9,19 +9,19 @@ token = os.getenv('TOKEN')
 bot = telebot.TeleBot(token)
 
 command_help = types.BotCommand(command='help', 
-								   		description='список возможностей бота')
+								description='список возможностей бота')
 
 command_newrem = types.BotCommand(command='newrem', 
-								   		  description='новое напоминание')
+								description='новое напоминание')
 
 command_remlist = types.BotCommand(command='remlist', 
-								   		  description='список напоминаний')
+								description='список напоминаний')
 
 command_delrem = types.BotCommand(command='delrem', 
-								   		  description='удалить напоминание')
+								description='удалить напоминание')
 
 command_delall = types.BotCommand(command='delall', 
-								   		  description='удалить все напоминания')
+								description='удалить все напоминания')
 
 list_commands = [command_newrem, command_remlist, 
-				 command_delrem, command_delall]
+				command_delrem, command_delall]
