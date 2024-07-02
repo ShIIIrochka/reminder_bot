6d544ca17d40ed2899e7b6fc0cbc9fb544773a23
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-02 22:54:49 +0300

    [+] пишу заново /start, добавила .env и .env.example

--- kernel/commands/help_start.py
+++ kernel/commands/help_start.py
@@ -1,13 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import telebot
-from constants import list_commands
-from constants import bot
 
-def func_help(message):
-	response = 'Привет! Я - бот-менеджер для напоминаний :)!\nВот что я могу:\n'
-	for command in list_commands:
-		response += f'/{command.command} - {command.description}\n'
-	bot.send_message(message.from_user.id, response)
+from ..constants import bot
 
-def send_list_of_commands(message):
-	if telebot.util.is_command(message.text) == False:
-		func_help(message)
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.from_user.id, "")
