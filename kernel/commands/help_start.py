f866aaf78c410a9a627a7b5661476319e5e0621f
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-03 15:39:09 +0300

    [+] добавила запись пользователя в бд при комманде /start

--- kernel/commands/help_start.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import telebot
-
-from ..constants import bot
-
-@bot.message_handler(commands = ['start'])
-def start(message):
-    bot.send_message(message.from_user.id, "")
--- kernel/main.py
+++ kernel/main.py
@@ -1,33 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import telebot
+
 from constants import bot
+from kernel.commands.start import start_
 
-from commands.newrem import  newrem_command
-from commands.newrem import callback_worker
-from commands.newrem import report
-
-from commands.help_start import func_help
-from commands.help_start import send_list_of_commands
-
-bot_token = telebot.TeleBot(bot)
-
-@bot.message_handler(commands=['help', 'start'])
-def handle_help(message):
-    func_help(message)
-
-@bot.message_handler(content_types=['text'])
-def not_command(message):
-    send_list_of_commands(message)
-
-def handle_newrem(message):
-    newrem_command(message)
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    callback_worker(call)
-
-@bot.message_handler(content_types=['text'])
-def date(message):
-    report(message)
+@bot.message_handler(commands=['start'])
+def start(message):
+    start_(message)
         
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
