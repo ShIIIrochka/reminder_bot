65401979256a23cb3b1a5f4c5dc9ccf4beb84415
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-05 14:30:29 +0300

    [+] добавила функцию обработки даты напоминания и сохранения напоминания в бд

--- kernel/commands/newrem.py
+++ kernel/commands/newrem.py
@@ -6,7 +6,7 @@ import threading
 from datetime import datetime
 
 from constants import bot
-from models import Reminder
+from models import Reminder, User
 from models.users import session
 
 class NewReminder():
@@ -37,3 +37,35 @@ class NewReminder():
         self.user_data['description'] = message.text
 
         bot.send_message(self.chat_id, "Введите дату напоминания в формате YYYY-MM-DD HH:MM")
+        bot.register_next_step_handler(message, self.process_date_step)
+
+    def process_date_step(self, message):
+        '''функция для обработки даты напоминания'''
+        
+        date_str = message.text
+        try:
+            reminder_date = datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+            self.user_data['date'] = reminder_date
+
+            # Сохранение напоминания в базу данных
+            telegram_id = message.from_user.id
+            user = session.query(User).filter_by(telegram_id=telegram_id).first()
+            if not user:
+                user = User(telegram_id=telegram_id)
+                session.add(user)
+                session.commit()
+            
+            new_reminder = Reminder(
+                name=self.user_data['name'],
+                description=self.user_data['description'],
+                date=reminder_date,
+                owner=user
+            )
+            session.add(new_reminder)
+            session.commit()
+
+            bot.send_message(self.chat_id, "Напоминание успешно создано!")
+
+        except ValueError:
+            bot.send_message(self.chat_id, "Некорректный формат даты. Пожалуйста, введите дату и время в формате 'YYYY-MM-DD HH:MM':")
+            bot.register_next_step_handler(message, self.process_date_step)
