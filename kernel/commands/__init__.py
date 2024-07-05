0d659c3ecebfea6b76a8d7b1ad91a7578792109a
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-05 15:13:17 +0300

    [+] добавила исполнение класса нового напоминания в main.py

--- kernel/commands/__init__.py
+++ kernel/commands/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from .start import start_
+from .newrem import NewReminder
--- kernel/main.py
+++ kernel/main.py
@@ -2,12 +2,18 @@
 
 import telebot
 
-from constants import bot
 from commands.start import start_
+from commands import NewReminder
+from constants import bot 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     start_(message)
-        
+
+@bot.message_handler(commands=['newrem'])
+def handle_newrem_command(message):
+    new_reminder = NewReminder()
+    new_reminder.newrem_command(message)
+
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
