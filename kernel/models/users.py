fd0c84988160cd417ec1bf7b4f82d219509a8178
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-10 16:38:39 +0300

    [~] кодстайл

--- kernel/models/users.py
+++ kernel/models/users.py
@@ -9,24 +9,23 @@ from services import engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
-    
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     telegram_id = Column(Integer, unique=True, nullable=False)
-    
     reminders = relationship("Reminder", back_populates="owner")
+
 
 class Reminder(Base):
     __tablename__ = 'reminders'
-    
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(Text(50))
     description = Column(Text)
     owner_id = Column(Integer, ForeignKey('users.id'))
     date = Column(DateTime)
-    
     owner = relationship("User", back_populates="reminders")
+
 
 # Создание таблиц в базе данных
 Base.metadata.create_all(engine)
