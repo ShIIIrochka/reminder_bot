05ce648955278da9852c73539b8693e3b5ebaf59
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-10 16:41:42 +0300

    [~] кодстайл

--- kernel/main.py
+++ kernel/main.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import telebot
-
 from commands.start import start_
 from commands.newrem import newrem_command
-from constants import bot 
+from constants import bot
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     start_(message)
 
+
 @bot.message_handler(commands=['newrem'])
 def handle_newrem_command(message):
     newrem_command(message)
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
