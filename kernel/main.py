6ac1296dc90a5b4fe112516153bebbcf235d9476
ShIIIrochka <shiroooooo@yandex.ru>
2024-07-16 20:18:57 +0300

    [+] добавила кнопки с выбором напоминания по названию

--- kernel/main.py
+++ kernel/main.py
@@ -3,6 +3,7 @@
 from services.start import start_
 from services.newrem import newrem_command
 from services.listrem import listrem_command
+from services.delrem import delrem_command
 from constants import bot
 from services.engine_service import Base, engine
 
@@ -22,6 +23,11 @@ def handle_newrem_command(message):
 @bot.message_handler(commands=['listrem'])
 def handle_listrem_command(message):
     listrem_command(message)
+
+
+@bot.message_handler(commands=['delrem'])
+def handle_delrem_command(message):
+    delrem_command(message)
 
 
 if __name__ == "__main__":
--- kernel/services/delrem.py
+++ kernel/services/delrem.py
@@ -1,8 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from constants import bot
+from telebot import types
+from sqlalchemy import select
 
+from constants import bot
+from models.users import Reminder
+from services.engine_service import session
 
 @bot.message_handler(commands=['listrem'])
 def delrem_command(message):
-    pass
+    markup = types.InlineKeyboardMarkup()
+    telegram_id = message.from_user.id
+    reminders = select(Reminder).where(Reminder.owner_id == telegram_id)
+    results = session.scalars(reminders).all()
+
+    for result in results:
+
+        key = types.InlineKeyboardButton(
+            text=f"{result.name}",
+            callback_data='delete'
+            )
+
+        markup.add(key)
+
+    bot.send_message(
+        message.chat.id,
+        "выбери напоминание для удаления",
+        reply_markup=markup
+        )
